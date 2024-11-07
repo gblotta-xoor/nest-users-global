2a81b9832c45b787307383a9e591fbf9f25a6114
Gonzalo Blotta <gonzalo.blotta@mi.unc.edu.ar>
2024-11-07 14:19:33 -0300

    feat (config): added configuration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
+import configuration from './config/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb+srv://blotin:XzoeUwBOKof3MA2I@cluster0.p49ri.mongodb.net/Cluster0?retryWrites=true&w=majority',
-      synchronize: true, 
-      entities: [User], 
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get('database.url'),
+        synchronize: true,
+        entities: [User],
+        logging: true,
+      }),
+      inject: [ConfigService]
     }),
     UsersModule,
   ],
